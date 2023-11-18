deab71e6bc0eaefa48b4728b00ee667aa496bb5d
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-11-18 09:59:25 +0300

    Create Calculator.h

---
+++ Week1/calculator_src/Calculator.h
@@ -0,0 +1,15 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    float addition(float a, float b);
+    float subtraction(float a, float b);
+    float multiplication(float a, float b);
+    float division(float a, float b);
+    float square(float a);
+    float exponentiation(float base, float exponent);
+    float modulus(float a, float b);
+};
+
+#endif // CALCULATOR_H
